031519827c6a486cb42de4fb2e86d46143d36891
MykeSelivanov <michael777selivanov@gmail.com>
2021-01-05 20:45:44 -0600

    Ticketing project ORM

--- src/main/java/com/cybertek/implementation/ProjectServiceImpl.java
+++ src/main/java/com/cybertek/implementation/ProjectServiceImpl.java
@@ -1,13 +1,30 @@
 package com.cybertek.implementation;
 
 import com.cybertek.dto.ProjectDTO;
+import com.cybertek.entity.Project;
+import com.cybertek.enums.Status;
+import com.cybertek.mapper.ProjectMapper;
+import com.cybertek.mapper.UserMapper;
+import com.cybertek.repository.ProjectRepository;
 import com.cybertek.service.ProjectService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class ProjectServiceImpl implements ProjectService {
+
+    private ProjectMapper projectMapper;
+    private ProjectRepository projectRepository;
+    private UserMapper userMapper;
+
+    @Autowired
+    public ProjectServiceImpl(ProjectMapper projectMapper, ProjectRepository projectRepository, UserMapper userMapper) {
+        this.projectMapper = projectMapper;
+        this.projectRepository = projectRepository;
+        this.userMapper = userMapper;
+    }
 
     @Override
     public ProjectDTO getByProjectCode(String code) {
@@ -20,8 +37,11 @@ public class ProjectServiceImpl implements ProjectService {
     }
 
     @Override
-    public ProjectDTO save(ProjectDTO dto) {
-        return null;
+    public void save(ProjectDTO dto) {
+        dto.setProjectStatus(Status.OPEN);
+        Project projectEntity = projectMapper.convertToEntity(dto);
+        projectEntity.setAssignedManager(userMapper.convertToEntity(dto.getAssignedManager()));
+        projectRepository.save(projectEntity);
     }
 
     @Override
