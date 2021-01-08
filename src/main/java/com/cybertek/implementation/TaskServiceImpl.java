fd977d8b4dfeecd346203e48a9fde7db776a9488
MykeSelivanov <michael777selivanov@gmail.com>
2021-01-08 16:59:09 -0600

    Ticketing project ORM

--- src/main/java/com/cybertek/implementation/TaskServiceImpl.java
+++ src/main/java/com/cybertek/implementation/TaskServiceImpl.java
@@ -2,13 +2,29 @@ package com.cybertek.implementation;
 
 import com.cybertek.dto.TaskDTO;
 import com.cybertek.entity.Task;
+import com.cybertek.enums.Status;
+import com.cybertek.mapper.TaskMapper;
+import com.cybertek.repository.TaskRepository;
 import com.cybertek.service.TaskService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class TaskServiceImpl implements TaskService {
+
+    TaskRepository taskRepository;
+    TaskMapper taskMapper;
+
+    @Autowired
+    public TaskServiceImpl(TaskRepository taskRepository, TaskMapper taskMapper) {
+        this.taskRepository = taskRepository;
+        this.taskMapper = taskMapper;
+    }
+
     @Override
     public TaskDTO findById(Long id) {
         return null;
@@ -16,12 +32,17 @@ public class TaskServiceImpl implements TaskService {
 
     @Override
     public List<TaskDTO> listAllTasks() {
-        return null;
+        List<Task> taskEntitiesList = taskRepository.findAll();
+//      return taskEntitiesList.stream().map(taskEntity -> { return taskMapper.convertToDTO(taskEntity);}).collect(Collectors.toList());
+        return taskEntitiesList.stream().map(taskMapper::convertToDTO).collect(Collectors.toList());
     }
 
     @Override
     public Task save(TaskDTO dto) {
-        return null;
+        dto.setTaskStatus(Status.OPEN);
+        dto.setAssignedDate(LocalDate.now());
+        Task taskEntity = taskMapper.convertToEntity(dto);
+        return taskRepository.save(taskEntity);
     }
 
     @Override
