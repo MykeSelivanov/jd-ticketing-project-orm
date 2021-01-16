b8beb613f75b8ec3e22a3ba9ba01397889aca098
MykeSelivanov <michael777selivanov@gmail.com>
2021-01-15 22:39:43 -0600

    Ticketing project ORM

--- src/main/java/com/cybertek/controller/TaskController.java
+++ src/main/java/com/cybertek/controller/TaskController.java
@@ -67,7 +67,6 @@ public class TaskController {
 
     @PostMapping("/update/{id}")
     public String editTask(@PathVariable("id") Long id, TaskDTO task, Model model) {
-
         taskService.update(task);
 
         return "redirect:/task/create";
--- src/main/java/com/cybertek/repository/TaskRepository.java
+++ src/main/java/com/cybertek/repository/TaskRepository.java
@@ -18,7 +18,7 @@ public interface TaskRepository extends JpaRepository<Task,Long> {
 
     @Query(value = "SELECT COUNT(*)" +
             " FROM tasks t JOIN projects p on t.project_id=p.id" +
-            " WHERE p.project_code = ?1 AND t.task_status = 'COMPLETED'", nativeQuery = true)
+            " WHERE p.project_code = ?1 AND t.task_status = 'COMPLETE'", nativeQuery = true)
     int totalCompletedTasks(String projectCode);
 
     List<Task> findAllByProject(Project project);
